7fd43723b9774406cf389334b74aa5a79914f8f8
ilyas.zelloufi@epitech.eu <zelloufi.ilyas@gmail.com>
2020-02-11 00:36:30 +0100

    bench text ok

--- src/App.js
+++ src/App.js
@@ -6,24 +6,45 @@ class App extends React.Component {
     super(props)
     this.state = {
       value: 0,
-      values: [],
     };
-    this.incrementValue = this.incrementValue.bind(this);
+    this.updateTxtPerf = [];
     this.clockPerformance = this.clockPerformance.bind(this);
+    this.getBenchmark = this.getBenchmark.bind(this);
+    this.getAvg = this.getAvg.bind(this);
+    this.incrementValues = this.incrementValues.bind(this);
   }
 
   clockPerformance(profilerId, mode, actualTime, baseTime, startTime, commitTime) {
-    console.log({ profilerId, mode, actualTime, baseTime, startTime, commitTime });
+    // console.log({ profilerId, mode, actualTime, baseTime, startTime, commitTime });
+    this.updateTxtPerf.push(actualTime)
   }
 
-  incrementValue() {
+  getAvg(perfs) {
+    const sum = perfs.reduce((a, b) => a + b, 0);
+    return (sum / perfs.length) || 0;
+  }
+
+  incrementValues() {
+    const promises = [];
+
     for (let i = 0; i < 1000; i++) {
-      setTimeout(() => {
+      promises.push(new Promise((resolve) => setTimeout(() => {
         this.setState(() => ({
           value: this.state.value + 1,
         }));
-      }, 0);
+        resolve();
+      }, 0)));
     }
+    return promises;
+  }
+
+  getBenchmark() {
+    Promise.all(this.incrementValues()).then(() => {
+      console.log('txt perf: ', Math.min(...this.updateTxtPerf), ' : ', Math.max(...this.updateTxtPerf), ' : ', this.getAvg(this.updateTxtPerf));
+      this.setState(() => ({ value: 0 }));
+      this.updateTxtPerf = []
+    })
+    // console.log('txt perf: ', Math.min(...this.updateTxtPerf), ' : ', Math.max(...this.updateTxtPerf), ' : ', this.getAvg(this.updateTxtPerf))
   }
 
   render() {
@@ -32,7 +53,7 @@ class App extends React.Component {
         <button
           ref={ref => (this.btnRef = ref)}
           className="button"
-          onClick={this.incrementValue}
+          onClick={this.getBenchmark}
         >
           increment
         </button>
